9b784df227f64546beab2ac74c852b5cc275b85f
Jeremie <taboada.jeremie@gmail.com>
2012-12-05 12:14:48 +0100

    Relooking :)

---
+++ font/Formfoundcom_Readme_09.txt
@@ -0,0 +1,5 @@
+"Formfound.com"
+(c) 2009 by Peter Fritzsche
+
+The personal, non-commercial use of my font is free.
+For other purposes, please contact me (illest_trader@web.de). You have to!!!
--- js/demo.js
+++ js/demo.js
@@ -1,76 +1,85 @@
+/**
+ * Animation examples.
+ */
 
-// Affine
+var examples = {
+  affine: function (x) { return 10 * x + 10 },
+  polynome: function (x) { return 5 * x * x + 10 },
+  cosinus: function (x) { return Math.cos(x * 84) * 66 },
+  tangente: function (x) { return Math.tan(x * 21) * 21 },
+  droite: { type: 'geo', n: 60, pas: 1 },
+  saw: { type: 'geo', n: 60, pas: -1 }
+}
 
-$( '#affine' ).origami({
-  width: 400,
-  height: 400,
-  frags: 10,
-  f: function (x) {
-    return 10 * x + 10;
+/**
+ * Get the animation for a specific example.
+ */
+
+function getAnim(index, debug) {
+  var src = debug ? "img/white.png" : "http://lorempixel.com/400/400";
+  $('#example').html('<div class="back"></div><img src="' + src + '">');
+
+  var ex = examples[index];
+  switch (typeof ex) {
+    case "function": // With equation :
+      var str = "f(x) =";
+      str += /\{.+return(.+);\}/.exec(ex.toString().replace(/\n/g, ""))[1];
+      $('#example .back').html('<div>' + str + '</div>');
+      return $('#example').origami({
+        width: 400,
+        height: 400,
+        frags: 10,
+        shadows: true,
+        f: ex
+      })
+    case "object": // Or suites :
+      var str = ex.type + ": n = " + ex.n + ", pas = " + ex.pas;
+      $('#example .back').html('<div>' + str + '</div>');
+      return $('#example').origami({
+        width: 400,
+        height: 400,
+        frags: 10,
+        shadows: true,
+        suite: {
+          type: ex.type,
+          n: ex.n,
+          pas: ex.pas
+        }
+      })
+    default: // Else...
+      return console.warn("Invalid example.");
   }
+}
+
+// default
+
+var anim = getAnim($('#examples').val(), debug)
+  , debug = false
+
+/**
+ * Switch debug.
+ */
+
+$('#debug').on('click', function () {
+  debug = !debug;
 })
 
-// Polynome
+/**
+ * Switch animation.
+ */
 
-$( '#polynome' ).origami({
-  width: 400,
-  height: 400,
-  frags: 10,
-  f: function (x) {
-    return 5 * x * x + 10;
-  }
+$('#examples').on('change', function () {
+  anim = getAnim(this.value, debug);
+});
+
+/**
+ * Trigger animation
+ */
+
+$("#on").on('click', function () {
+  anim.on();
+});
+
+$('#off').on('click', function () {
+  anim.off();
 })
-
-// cosinus
-
-$( '#cosinus' ).origami({
-  width: 400,
-  height: 400,
-  frags: 10,
-  f: function (x) {
-    var eq = Math.cos(x * 84) * 66;
-    return eq;
-  }
-});
-
-// tangente (gros bordel)
-
-$( '#tangente' ).origami({
-  width: 400,
-  height: 400,
-  frags: 10,
-  f: function (x) {
-    return Math.tan(x * 21) * 21;
-  },
-  shadows: true // voir pourquoi ca bug ici...
-});
-
-// Suite 'droite'
-
-$( '#droite' ).origami({
-  width: 400,
-  height: 400,
-  frags: 10,
-  suite: {
-    type: 'geo',
-    n: 60,
-    pas: 1
-  },
-  shadows: true,
-  delay: 10
-});
-
-// Suite 'dents de scie'
-
-$( '#saw' ).origami({
-  width: 400,
-  height: 400,
-  frags: 10,
-  suite: {
-    type: 'geo',
-    n: 60,
-    pas: -1
-  },
-  shadows: true,
-  delay: 20
-});
