162dca3077f0dbffbb37cdc7ec5416928e2a45aa
Jeremie <taboada.jeremie@gmail.com>
2012-12-17 16:58:06 +0100

    Update origami to the latest version

--- demo/demo.js
+++ demo/demo.js
@@ -1,9 +1,11 @@
-var anim = $("#picture").origami({
-  shadows: false,
-  frags: 10,
-  f: function (x) {
-    return Math.sin(x * 42) * 84 + 21
-  }
-})
+if ($("#picture").length) {
+  var anim = $("#picture").origami({
+    shadows: false,
+    frags: 10,
+    f: function (x) {
+      return Math.sin(x * 42) * 84 + 21
+    }
+  })
 
-$('#picture').hover(anim.on, anim.off);
+  $('#picture').hover(anim.on, anim.off);
+}
