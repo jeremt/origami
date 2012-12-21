227cad898ee921801b2dfd2a9d6ad7434fb12c23
JeremieT <jeremie@kontestapp.com>
2012-12-21 21:56:12 +0100

    fix for asynchronous stuffs

--- demo/demo.js
+++ demo/demo.js
@@ -1,11 +1,32 @@
-if ($("#picture").length) {
-  var anim = $("#picture").origami({
-    shadows: false,
-    frags: 10,
-    f: function (x) {
-      return Math.sin(x * 42) * 84 + 21
-    }
-  })
+$(function () {
 
-  $('#picture').hover(anim.on, anim.off);
-}
+  if (!$.fn.origami.enabled)
+    alert('Css3d isnt enabled on your browser :(')
+
+  if ($("#picture").length) {
+
+    $("#picture").origami({
+      shadows: false,
+      frags: 10,
+      f: function (x) {
+        return Math.sin(x * 42) * 84 + 21
+      }
+    }, function (anim) {
+      $('#picture').hover(anim.on, anim.off)
+    })
+
+  } else if ($('#debug').length) {
+
+    $('#debug').origami({
+      shadows: true,
+      frags: 10,
+      f: function (x) {
+        return 10 * x
+      }
+    }, function (anim) {
+      $('#debug').hover(anim.on, anim.off)
+    })
+
+  }
+
+})
