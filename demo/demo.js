e7ed8a6ba8291c0995949cbf82489fc9729e8505
JeremieT <jeremie@kontestapp.com>
2012-12-22 01:18:56 +0100

    bug on refresh

--- demo/demo.js
+++ demo/demo.js
@@ -1,32 +1,29 @@
-$(function () {
 
-  if (!$.fn.origami.enabled)
-    alert('Css3d isnt enabled on your browser :(')
+if (!$.fn.origami.enabled)
+  alert('Css3d isnt enabled on your browser :(')
 
-  if ($("#picture").length) {
+if ($("#picture").length) {
 
-    $("#picture").origami({
-      shadows: false,
-      frags: 10,
-      f: function (x) {
-        return Math.sin(x * 42) * 84 + 21
-      }
-    }, function (anim) {
-      $('#picture').hover(anim.on, anim.off)
-    })
+  $("#picture").origami({
+    shadows: false,
+    frags: 10,
+    f: function (x) {
+      return Math.sin(x * 42) * 84 + 21
+    }
+  }, function (anim) {
+    $('#picture').hover(anim.on, anim.off)
+  })
 
-  } else if ($('#debug').length) {
+} else if ($('#debug').length) {
 
-    $('#debug').origami({
-      shadows: true,
-      frags: 10,
-      f: function (x) {
-        return 10 * x
-      }
-    }, function (anim) {
-      $('#debug').hover(anim.on, anim.off)
-    })
+  $('#debug').origami({
+    shadows: true,
+    frags: 10,
+    f: function (x) {
+      return Math.cos(x * 84) * 21
+    }
+  }, function (anim) {
+    $('#debug').hover(anim.on, anim.off)
+  })
 
-  }
-
-})
+}
